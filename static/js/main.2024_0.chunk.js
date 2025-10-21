dacc92df6d41d2b526f43184c031fc26de00181c
twwwwwwwsokollovss <156020917+APPLEX1x@users.noreply.github.com>
2025-10-21 17:45:50 +0200

    Add files via upload

---
+++ static/js/main.2024_0.chunk.js
@@ -0,0 +1,44 @@
+Object(g.jsxs)(o.l, {
+    children: [
+        Object(g.jsx)(I, {
+            title: "Телеграм бот AutoStars",
+            job: "FullStack Developer",
+            work: "2025now",
+            odnoras: "❌",
+            telegram: "#"
+        }),
+        Object(g.jsx)(I, {
+            title: "Телеграм бот AutoShop",
+            job: "Backend Developer",
+            work: "2023now", 
+            odnoras: "❌",
+            telegram: "#"
+        }),
+        Object(g.jsx)(I, {
+            title: "DDoS панель jsonetwork.cc",
+            job: "FullStack Developer",
+            work: "2022now",
+            odnoras: "❌",
+            telegram: "#"
+        }),
+        Object(g.jsx)(I, {
+            title: "Авторегер VK/TG",
+            job: "Owner",
+            work: "2024now",
+            odnoras: "❌", 
+            telegram: "#"
+        }),
+        Object(g.jsx)(Y, {
+            title: "Фишинг панель «PhishNet»",
+            job: "Front Developer",
+            odnoras: "✅",
+            telegram: "#"
+        }),
+        Object(g.jsx)(Y, {
+            title: "Защита ShielGuard для .NET приложений",
+            job: "Owner", 
+            odnoras: "✅",
+            telegram: "#"
+        })
+    ]
+})
